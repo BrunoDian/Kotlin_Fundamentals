f0cfb6ad424a28c01bd96070853d1ba234c789ad
Bruno <bruno.lo.dev@gmail.com>
2022-11-03 20:51:24 -0300

    First steps with 'main' function and comments.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,25 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+/* What is Kotlin?
+ * It's a modern programming language, used for:
+ * Web development;
+ * Server side applications;
+ * Data Science;
+ * Declared by Google as the main development language for Android;
+ * And now it's in Beta for Multiplatform Mobile, when you share the logic of your iOS and Android apps.
+ */
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(){
+    // This is the 'main' Function, all projects (and classes) need to have one to work,
+    // this is the entry point to the compiler construct your project.
 }
+
+// ~~~~~~~~~~ START -> COMMENTS ~~~~~~~~~~ //
+
+    // comments in Kotlin are made with two ways:
+    // <-- This is a Single Line comment.
+
+    /*
+    * And this is a multiline
+    * comment
+    */
+
+// ~~~~~~~~~~ STOP -> COMMENTS ~~~~~~~~~~ //
