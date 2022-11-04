e1e2b15cd681850226ebd256b5a445022bd7334c
Bruno <bruno.lo.dev@gmail.com>
2022-11-04 19:32:03 -0300

    Basic explanations about JDK and JVM.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+
 /* What is Kotlin?
  * It's a modern programming language, used for:
  * Web development;
@@ -5,10 +6,17 @@
  * Data Science;
  * Declared by Google as the main development language for Android;
  * And now it's in Beta for Multiplatform Mobile, when you share the logic of your iOS and Android apps.
+ *
+ * How can Kotlin do all this? Because of JDK and JVM.
+ * The Kotlin code that you write is compiled by the JDK (Java Development Kit) to another code called
+ * 'Java bytecode' and it's an intermediate of high level code and a machine code.
+ * This 'Java bytecode' is read by the JVM (Java Virtual Machine) and converted to a Machine Code, adding
+ * instructions about the processor and system operational.
+ * This means that every system that has a JVM can read Kotlin code!
  */
 
 fun main(){
-    // This is the 'main' Function, all projects (and classes) need to have one to work,
+    // This is the 'main' function, all projects (and classes) need to have one to work,
     // this is the entry point to the compiler construct your project.
 }
 
